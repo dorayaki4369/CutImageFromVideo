c39c541f36ea3c74b8a30db2e948a391c92f6369
dorayaki4369 <dorayaki4369@outlook.jp>
2017-07-08 22:06:24 +0900

    Make basic screen

--- App.xaml.cs
+++ App.xaml.cs
@@ -6,12 +6,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace CutImageFromMovie
-{
+namespace CutImageFromMovie {
     /// <summary>
     /// App.xaml の相互作用ロジック
     /// </summary>
-    public partial class App : Application
-    {
-    }
+    public partial class App : Application { }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,16 +13,15 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace CutImageFromMovie
-{
+namespace CutImageFromMovie {
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+    public partial class MainWindow : Window {
+        public MainWindow() {
             InitializeComponent();
         }
+
+
     }
 }
