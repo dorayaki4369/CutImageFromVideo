da457d9646f9abcfe255f6c03661c7714c4015d9
dorayaki4369 <dorayaki4369@outlook.jp>
2017-07-22 15:47:10 +0900

    Add error check function

--- CutImageFromVideo/SettingData.cs
+++ CutImageFromVideo/SettingData.cs
@@ -1,15 +1,23 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.IO;
+using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Windows.Forms;
 using CutImageFromVideo.Annotations;
 
 namespace CutImageFromVideo {
-    public class SettingData : INotifyPropertyChanged {
+    public class SettingData : INotifyPropertyChanged, INotifyDataErrorInfo {
+        /**
+         * INotifyPropertyChanged
+        */
         public event PropertyChangedEventHandler PropertyChanged;
 
         private void SetProperty<T>([NotNull] ref T field, [NotNull] T value,
-                                    [CallerMemberName] string propertyName = null) {
+            [CallerMemberName] string propertyName = null) {
             //if (field == null) throw new ArgumentNullException(nameof(field));
 
             field = value;
@@ -17,20 +25,72 @@ namespace CutImageFromVideo {
             h?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
+        /**
+         * INotifyErrorsInfo
+         */
+        private Dictionary<string, IEnumerable> errors = new Dictionary<string, IEnumerable>();
+
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+        private void OnErrorsChanged([CallerMemberName] string propertyName = null) {
+            var h = ErrorsChanged;
+            h?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
+        }
+
+        public IEnumerable GetErrors(string propertyName) {
+            IEnumerable error;
+            errors.TryGetValue(propertyName, out error);
+            return error;
+        }
+
+        public bool HasErrors {
+            get { return errors.Values.Any(e => e != null); }
+        }
+
+        /**
+         * Fields
+         */
         public ObservableCollection<string> VideoFileNames { get; set; }
 
         private string _cascadeFileName;
 
         public string CascadeFileName {
             get { return _cascadeFileName; }
-            set { SetProperty(ref _cascadeFileName, value); }
+            set {
+                SetProperty(ref _cascadeFileName, value);
+                if (string.IsNullOrEmpty(value)) {
+                    errors["CascadeFileName"] = new[] {"Please enter a cascade file path"};
+                }
+                else if (!value.Contains(".xml")) {
+                    errors["CascadeFileName"] = new[] {"The file is not a cascade file"};
+                }
+                else if (!File.Exists(value)) {
+                    errors["CascadeFileName"] = new[] {"The file does not exist"};
+                }
+                else {
+                    errors["CascadeFileName"] = null;
+                }
+                OnErrorsChanged();
+            }
         }
 
         private string _outputDirectryName;
 
         public string OutputDirectryName {
             get { return _outputDirectryName; }
-            set { SetProperty(ref _outputDirectryName, value); }
+            set {
+                SetProperty(ref _outputDirectryName, value);
+                if (string.IsNullOrEmpty(value)) {
+                    errors["OutputDirectryName"] = new[] {"Please enter a directry path"};
+                }
+                else if (!Directory.Exists(value)) {
+                    errors["OutputDirectryName"] = new[] {"The directry does not exist"};
+                }
+                else {
+                    errors["OutputDirectryName"] = null;
+                }
+                OnErrorsChanged();
+            }
         }
 
         private int _totalFrameNum;
@@ -51,7 +111,16 @@ namespace CutImageFromVideo {
 
         public string ImageName {
             get { return _imageName; }
-            set { SetProperty(ref _imageName, value); }
+            set {
+                SetProperty(ref _imageName, value);
+                if (string.IsNullOrEmpty(value)) {
+                    errors["ImageName"] = new[] {"Please enter a image name"};
+                }
+                else {
+                    errors["ImageName"] = null;
+                }
+                OnErrorsChanged();
+            }
         }
 
         private string _imageExtention;
